6188dcd34c132c59aef3898eacc3f97552b37914
rvance1 <roystonvance@gmail.com>
2026-01-13 19:33:59 -0700

    added tiles to board and other

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,6 +8,8 @@ package chess;
  */
 public class ChessBoard {
 
+    ChessPiece[][] tiles = new ChessPiece[8][8];
+    
     public ChessBoard() {
         
     }
@@ -19,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        tiles[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +32,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return tiles[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
