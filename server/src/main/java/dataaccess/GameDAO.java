31b62e7deb4223cfb142fe68bdb4fb9448938175
rvance1 <roystonvance@gmail.com>
2026-02-25 14:28:04 -0700

    added game dao

---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,14 @@
+package dataaccess;
+
+import java.util.Collection;
+
+import model.GameData;
+
+public interface GameDAO {
+    void clear() throws DataAccessException;
+
+    int createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+}
