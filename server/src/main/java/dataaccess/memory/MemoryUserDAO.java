1ec6a98f75d7d4d8315b1a9969a536211e676b19
rvance1 <roystonvance@gmail.com>
2026-02-25 14:38:03 -0700

    added memory user dao

---
+++ server/src/main/java/dataaccess/memory/MemoryUserDAO.java
@@ -0,0 +1,36 @@
+package dataaccess.memory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.UserData;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+        if (user == null || user.username() == null) {
+            throw new DataAccessException("user/username cannot be null");
+        }
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("user already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        if (username == null) {
+            throw new DataAccessException("username cannot be null");
+        }
+        return users.get(username); // null if not found (normal)
+    }
+}
