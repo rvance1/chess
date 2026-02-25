b122f7bdc57f9203e9b8a982f3d05e08bdb4e80a
rvance1 <roystonvance@gmail.com>
2026-02-25 14:47:13 -0700

    finished memory auth dao

--- server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
@@ -17,16 +17,28 @@ public class MemoryAuthDAO implements AuthDAO {
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
-
+        if (auth == null || auth.authToken() == null || auth.username() == null) {
+            throw new DataAccessException("authToken/username cannot be null");
+        }
+        if (tokens.containsKey(auth.authToken())) {
+            throw new DataAccessException("auth token already exists");
+        }
+        tokens.put(auth.authToken(), auth);
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-
+        if (authToken == null) {
+            throw new DataAccessException("authToken cannot be null");
+        }
+        return tokens.get(authToken); // null if not found
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
-
+        if (authToken == null) {
+            throw new DataAccessException("authToken cannot be null");
+        }
+        tokens.remove(authToken); // ok even if absent
     }
 }
