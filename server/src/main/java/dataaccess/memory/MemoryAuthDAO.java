ec859962cd96e37e6b69e23af8f40f9cbd05cc4b
rvance1 <roystonvance@gmail.com>
2026-02-25 14:45:44 -0700

    memory auth dao

--- server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
@@ -1,8 +1,32 @@
 package dataaccess.memory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import model.AuthData;
 
-import java.util.HashMap;
-import java.util.Map;
+public class MemoryAuthDAO implements AuthDAO {
+    private final Map<String, AuthData> tokens = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        tokens.clear();
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+}
