bac4ac92c81a82621748281ddd094ba04e25409c
rvance1 <roystonvance@gmail.com>
2026-02-28 13:25:16 -0700

    added clear handler

---
+++ server/src/main/java/handler/ClearHandler.java
@@ -0,0 +1,26 @@
+package handler;
+
+import java.util.Map;
+
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import service.ClearService;
+
+public class ClearHandler {
+    private final ClearService clearService;
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    public void handle(Context ctx) {
+        try {
+            clearService.clear();
+            ctx.status(200);
+            ctx.json(Map.of());
+        } catch (DataAccessException e) {
+            ctx.status(500);
+            ctx.json(Map.of("message", "Error: " + e.getMessage()));
+        }
+    }
+}
