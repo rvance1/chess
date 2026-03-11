e7bb818a3d190511e6b5dd5829677a50c6a84c92
rvance1 <roystonvance@gmail.com>
2026-03-11 11:26:50 -0600

    fixed exception

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -1,7 +1,11 @@
 package dataaccess;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import exception.DataAccessException;
 
 public class DatabaseManager {
     private static String databaseName;
