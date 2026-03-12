a6bc5fd04f95a0b222c6fa4855e628e02a31619a
rvance1 <roystonvance@gmail.com>
2026-03-11 18:17:07 -0600

    added sql auth dao

---
+++ server/src/main/java/dataaccess/sql/SqlAuthDAO.java
@@ -0,0 +1,82 @@
+package dataaccess.sql;
+
+import exception.DataAccessException;
+import model.AuthData;
+import dataaccess.AuthDAO;
+import dataaccess.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SqlAuthDAO implements AuthDAO {
+
+    public SqlAuthDAO() throws DataAccessException {
+        DatabaseManager.configureDatabase();
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        var statement = "TRUNCATE TABLE auth";
+        executeUpdate(statement);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        var statement = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        executeUpdate(statement, auth.authToken(), auth.username());
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        var statement = "SELECT authToken, username FROM auth WHERE authToken = ?";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, authToken);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthData(
+                            rs.getString("authToken"),
+                            rs.getString("username")
+                    );
+                }
+            }
+
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get auth", ex);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        var statement = "DELETE FROM auth WHERE authToken = ?";
+        executeUpdate(statement, authToken);
+    }
+
+    private void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+
+            for (int i = 0; i < params.length; i++) {
+                if (params[i] instanceof String s) {
+                    ps.setString(i + 1, s);
+                } else if (params[i] == null) {
+                    ps.setString(i + 1, null);
+                } else {
+                    ps.setObject(i + 1, params[i]);
+                }
+            }
+
+            ps.executeUpdate();
+
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to update auth table", ex);
+        }
+    }
+}
