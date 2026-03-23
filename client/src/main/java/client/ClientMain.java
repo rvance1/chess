d80198dc14dc96b8dd0afb3e2760f2caedd87834
rvance1 <roystonvance@gmail.com>
2026-03-23 09:57:21 -0600

    fixed main

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,12 +2,14 @@ package client;
 
 import chess.ChessGame;
 import chess.ChessPiece;
+import ui.REPL;
 
 public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-    }
 
-    
+        REPL repl = new REPL("http://localhost:8080");
+        repl.run();
+    }
 }
